ac7b7e0d8b5dc0f6d0760dc1c275f659011878ab
Vijay Radha <polymorphic.technologies@gmail.com>
2024-03-13 22:15:39 +0000

    Backend test failure

--- starter/backend/test_app.py
+++ starter/backend/test_app.py
@@ -15,7 +15,7 @@ def test_movies_endpoint_returns_json():
         assert response.content_type == "application/json"
 
 
-def test_movies_endpoint_returns_valid_data():
+def testing_movies_endpoint_returns_valid_data():
     with app.test_client() as client:
         response = client.get("/movies/")
         data = response.get_json()
